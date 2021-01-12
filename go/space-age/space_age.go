418061c69653e4e251c4e425c65fee2cf35caa7a
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-12 23:29:35 +0900

    first solve space-age in go-track

---
+++ go/space-age/space_age.go
@@ -0,0 +1,19 @@
+package space
+
+type Planet string
+
+var planets = map[Planet]float64{
+	Planet("Mercury"): 0.2408467,
+	Planet("Venus"): 0.61519726,
+	Planet("Earth"): 1.0,
+	Planet("Mars"): 1.8808158,
+	Planet("Jupiter"): 11.862615,
+	Planet("Saturn"): 29.447498,
+	Planet("Uranus"): 84.016846,
+	Planet("Neptune"): 164.79132,
+}
+
+func Age(seconds float64, planet Planet) float64  {
+	var earthPeriodSecond float64 = 31557600
+	return seconds/ earthPeriodSecond /planets[planet]
+}
