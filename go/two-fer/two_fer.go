75aaa57ae62df97e60bbed059aaa091a6b4c42fa
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-16 13:19:07 +0900

    apply review comment

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -9,6 +9,8 @@ import "fmt"
 
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	if name == "" {name = "you"}
+	if name == "" {
+		name = "you"
+	}
 	return fmt.Sprintf("One for %s, one for me.", name)
 }
