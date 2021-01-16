52944fcf3e59fc0435b308fb52773bef964a3cd4
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-16 22:53:18 +0900

    refactor

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -7,8 +7,5 @@ package leap
 
 // IsLeapYear should have a comment documenting it.
 func IsLeapYear(year int) bool {
-	if year%4 == 0 && year%100 != 0 || year%400 == 0 {
-		return true
-	}
-	return false
+	return year%4 == 0 && year%100 != 0 || year%400 == 0
 }
