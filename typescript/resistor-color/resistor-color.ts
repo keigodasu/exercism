7663c1490a3d7eae25e099b2738a08e4eae62a30
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-22 22:30:25 +0900

    refactory

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,7 +1,3 @@
 export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green','blue', 'violet', 'grey', 'white']
 
-export const colorCode = (color: string): number =>  {
-    return COLORS.findIndex(function (element) {
-        return  element === color
-    })
-}
+export const colorCode = (color: string): number => COLORS.indexOf(color)
