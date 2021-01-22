2a7b388db891a69a213cee654ab6456bfa0fc8c1
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-22 20:01:19 +0900

    solve

---
+++ typescript/leap/leap.ts
@@ -0,0 +1,8 @@
+function isLeapYear(year: number): boolean {
+    if (year % 100 == 0 && year % 400 != 0) return false
+    if (year % 4 == 0) return true
+
+    return false
+}
+
+export default isLeapYear
