31ed9bf4605c94b0c275ad67dc5e53f512a4a15c
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-22 20:06:30 +0900

    use date

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,8 +1,5 @@
 function isLeapYear(year: number): boolean {
-    if (year % 100 == 0 && year % 400 != 0) return false
-    if (year % 4 == 0) return true
-
-    return false
+    return new Date(year, 2, 0).getDate() == 29
 }
 
 export default isLeapYear
