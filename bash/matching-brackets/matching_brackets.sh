27c558dc6f8b18875129206cf4b2cb87cb89a103
keigodasu <keigodasu05240524@gmail.com>
2021-02-08 23:48:48 +0900

    solve

---
+++ bash/matching-brackets/matching_brackets.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+declare -A brackets=(
+  ["]"]="["
+  [")"]="("
+  ["}"]="{"
+)
+stack=""
+
+for ((i=0; i<${#1}; i++)); do
+  char=${1:i:1}
+  case $char in
+    "[" | "(" | "{" )
+      stack+=$char
+      ;;
+    "]" | ")" | "}" )
+      if [[ -z $stack || $stack != *"${brackets[$char]}" ]]; then
+        echo false
+        exit
+      else
+        echo $stack
+        echo ${stack%?}
+        stack=${stack%?}
+      fi
+      ;;
+ esac
+done
+
+[[ -z $stack ]] && echo true || echo false
+
